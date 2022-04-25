6b5fc21ab7541204693ad977d6ad2919feade78b
Thomas Alrek <thomas@alrek.no>
2022-04-25 14:48:20 +0200

    Fix auth key

--- src/Netflex/Actions/Middlewares/WebhookAuthMiddleware.php
+++ src/Netflex/Actions/Middlewares/WebhookAuthMiddleware.php
@@ -2,36 +2,39 @@
 
 namespace Netflex\Actions\Middlewares;
 
+use Closure;
+
+use Carbon\Carbon;
+
+use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
-use Carbon\Carbon;
-use Closure;
-use Illuminate\Http\Request;
 
 class WebhookAuthMiddleware
 {
     /**
      * Handle an incoming request.
      *
-     * @param Request $r
+     * @param Request $request
      * @param Closure $next
      * @return mixed
      */
-    public function handle(Request $r, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
-        $nonce = $r->headers->get("X-Nonce");
-        $digest = $r->headers->get("X-Digest");
-        $ts = $r->headers->get("X-Timestamp");
+        $nonce = $request->headers->get('X-Nonce');
+        $digest = $request->headers->get('X-Digest');
+        $ts = $request->headers->get('X-Timestamp');
         $cTs = Carbon::parse($ts);
 
-        if (($digest && hash_hmac("SHA256", "$ts$nonce", config("api.privateKey")) === $digest && $cTs && abs($cTs->diffInSeconds(Carbon::now())) < 30) || App::environment() === "local") {
-            Cache::set("run-{$nonce}", true);
-            Log::debug("Authorized webhook", ['ts' => $ts, 'nonce' => $nonce, 'digest' => $digest]);
-            return $next($r);
+        if (($digest && hash_hmac('SHA256', '$ts$nonce', variable('netflex_api')) === $digest && $cTs && abs($cTs->diffInSeconds(Carbon::now())) < 30) || App::environment() === 'local') {
+            Cache::set('run-{$nonce}', true);
+            Log::debug('Authorized webhook', ['ts' => $ts, 'nonce' => $nonce, 'digest' => $digest]);
+            return $next($request);
         }
 
-        Log::debug("Unauthorized webhook", ['ts' => $ts, 'nonce' => $nonce, 'digest' => $digest]);
+        Log::debug('Unauthorized webhook', ['ts' => $ts, 'nonce' => $nonce, 'digest' => $digest]);
         return abort(401);
     }
 }
