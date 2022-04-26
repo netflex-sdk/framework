65ea8fcb6602de3a7d2488b6ec0af90834c5d279
Thomas Alrek <thomas@alrek.no>
2022-04-26 10:33:43 +0200

    Fix command provider loading

--- src/Netflex/Actions/Middlewares/WebhookAuthMiddleware.php
+++ src/Netflex/Actions/Middlewares/WebhookAuthMiddleware.php
@@ -22,20 +22,22 @@ class WebhookAuthMiddleware
     {
         $nonce = $request->headers->get('X-Nonce');
         $digest = $request->headers->get('X-Digest');
-        $ts = $request->headers->get('X-Timestamp');
-        $cTs = Carbon::parse($ts);
+        $timestamp = $request->headers->get('X-Timestamp');
+        $datetime = Carbon::parse($timestamp);
 
         $digestOk = $digest && hash_hmac('SHA256', '$ts$nonce', variable('netflex_api') === $digest);
-        $timeOk = $cTs && abs($cTs->diffInSeconds(Carbon::now())) < 30;
-        $notRunned = \Cache::get("run-$nonce", false) == false;
+        $timeOk = $datetime && abs($datetime->diffInSeconds(Carbon::now())) < 30;
+        $notRunned = Cache::get("run-$nonce", false) == false;
 
-        if (($digestOk && $timeOk && $notRunned) || App::environment() === 'local') {
-            Cache::set('run-{$nonce}', true);
-            Log::debug('Authorized webhook', ['ts' => $ts, 'nonce' => $nonce, 'digest' => $digest]);
+        $validated = ($digestOk && $timeOk && $notRunned) || !App::isProduction();
+
+        if ($validated) {
+            Cache::set("run-{$nonce}", true);
+            Log::debug('Authorized webhook', ['ts' => $timestamp, 'nonce' => $nonce, 'digest' => $digest]);
             return $next($request);
         }
 
-        Log::debug('Unauthorized webhook', ['ts' => $ts, 'nonce' => $nonce, 'digest' => $digest]);
+        Log::debug('Unauthorized webhook', ['ts' => $timestamp, 'nonce' => $nonce, 'digest' => $digest]);
         return abort(401);
     }
 }
