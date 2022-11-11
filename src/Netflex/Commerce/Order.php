57e365256dfee0e9e6e64c3606ae66730cd84602
Thomas Alrek <thomas@alrek.no>
2022-11-11 01:44:59 +0100

    Listen for events from all order models

--- src/Netflex/Commerce/Order.php
+++ src/Netflex/Commerce/Order.php
@@ -16,4 +16,74 @@ class Order extends AbstractOrder
     'locked',
     'checkout'
   ];
+
+  /**
+   * Register a model event with the dispatcher.
+   *
+   * @param  string  $event
+   * @param  \Illuminate\Events\QueuedClosure|\Closure|string  $callback
+   * @return void
+   */
+  protected static function registerModelEvent($event, $callback)
+  {
+    if (isset(static::$dispatcher)) {
+      $name = static::class;
+
+      static::$dispatcher->listen("netflex.commerce.order.{$event}*", $callback);
+    }
+  }
+
+  /**
+   * Fire the given event for the model.
+   *
+   * @param  string  $event
+   * @param  bool  $halt
+   * @return mixed
+   */
+  protected function fireModelEvent($event, $halt = true)
+  {
+    if (!isset(static::$dispatcher)) {
+      return true;
+    }
+
+    // First, we will get the proper method to call on the event dispatcher, and then we
+    // will attempt to fire a custom, object based event for the given event. If that
+    // returns a result we can return that result, or we'll call the string events.
+    $method = $halt ? 'until' : 'dispatch';
+
+    $result = $this->filterModelEventResults(
+      $this->fireCustomModelEvent($event, $method)
+    );
+
+    if ($result === false) {
+      return false;
+    }
+
+    return !empty($result) ? $result : static::$dispatcher->{$method}(
+      "netflex.commerce.order.{$event}: " . static::class,
+      $this
+    );
+  }
+
+  /**
+   * Remove all of the event listeners for the model.
+   *
+   * @return void
+   */
+  public static function flushEventListeners()
+  {
+    if (!isset(static::$dispatcher)) {
+      return;
+    }
+
+    $instance = new static;
+
+    foreach ($instance->getObservableEvents() as $event) {
+      static::$dispatcher->forget("netflex.commerce.order.{$event}: " . static::class);
+    }
+
+    foreach (array_values($instance->dispatchesEvents) as $event) {
+      static::$dispatcher->forget($event);
+    }
+  }
 }
