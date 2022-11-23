64bceeeb2e31282cd47d866fe2ba45d0be7b6a13
Thomas Alrek <thomas@alrek.no>
2022-11-23 16:03:59 +0100

    Update FoundationServiceProvider.php

--- src/Netflex/Foundation/Providers/FoundationServiceProvider.php
+++ src/Netflex/Foundation/Providers/FoundationServiceProvider.php
@@ -2,31 +2,29 @@
 
 namespace Netflex\Foundation\Providers;
 
-use Illuminate\Config\Repository;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Event;
+use Illuminate\Config\Repository as Cache;
 use Illuminate\Support\ServiceProvider;
 
 class FoundationServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        app('events')->listen("bootstrapping: Illuminate\Foundation\Bootstrap\BootProviders", function() {
-            $old = app('config')->all();
-            $this->app->instance('config', new Repository($this->replace_variables($old)));
+        app('events')->listen("bootstrapping: Illuminate\Foundation\Bootstrap\BootProviders", function () {
+            /** @var Cache $instance */
+            $instance = $this->app->make('config');
+            $this->replace_variables($instance, $instance->all());
         });
     }
 
-    private function replace_variables($config, $i = 0) {
-        if(is_array($config)) {
+    private function replace_variables(Cache $instance, $config, $keys = [])
+    {
+        if (is_array($config)) {
             return collect($config)
-                ->map(fn($value) => $this->replace_variables($value, $i + 1))
+                ->map(fn ($value, $key) => $this->replace_variables($instance, $value, array_merge($keys, [$key])))
                 ->toArray();
-        } if(is_string($config) && strlen($config) >= 3 && $config[0] === "#" && $config[strlen($config) - 1] === "#") {
-            return variable(substr($config, 1, -1));
-        } else {
-            return $config;
+        }
+        if (is_string($config) && strlen($config) >= 3 && $config[0] === "#" && $config[strlen($config) - 1] === "#") {
+            $instance->set(implode('.', $keys), variable(substr($config, 1, -1)));
         }
     }
 }
