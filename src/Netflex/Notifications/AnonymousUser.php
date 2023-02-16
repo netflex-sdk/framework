cd511196cbf83dea45e7184f9d3fc7e9749a164b
Thomas Alrek <thomas@alrek.no>
2023-02-16 14:56:04 +0100

    Add generic notification method

--- src/Netflex/Notifications/AnonymousUser.php
+++ src/Netflex/Notifications/AnonymousUser.php
@@ -5,22 +5,52 @@ namespace Netflex\Notifications;
 use Netflex\Customers\Customer;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * @property-read string $email
+ * @property-read string $phone
+ * @property-read string $phone_countrycode
+ */
 final class AnonymousUser extends Customer
 {
     use Notifiable;
 
-    public function __construct($to)
+    public function getEmailAttribute()
     {
-        parent::__construct();
-        $this->phone = $to;
+        return $this->mail;
     }
 
+    /**
+     * @param string|Notifiable $to
+     * @return AnonymousUser|Notifiable
+     */
     public static function make($to)
     {
         if (is_object($to) && in_array(Notifiable::class, class_uses_recursive($to))) {
             return $to;
         }
 
-        return new static($to);
+        return new static;
+    }
+
+    public static function fromMail($to): AnonymousUser
+    {
+        $user = static::make($to);
+
+        if ($user !== $to) {
+            $user->mail = $to;
+        }
+
+        return $user;
+    }
+
+    public static function fromPhone($to): AnonymousUser
+    {
+        $user = static::make($to);
+
+        if ($user !== $to) {
+            $user->phone = $to;
+        }
+
+        return $user;
     }
 }
