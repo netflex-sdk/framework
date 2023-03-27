38f110764feb8a751b616a5877b9b21e33a166a2
Olav Øyen <olav@apility.no>
2023-03-27 20:01:56 +0200

    Improves loading time of StructureServiceProvider

--- src/Netflex/Structures/Providers/StructureServiceProvider.php
+++ src/Netflex/Structures/Providers/StructureServiceProvider.php
@@ -6,6 +6,8 @@ namespace Netflex\Structure\Providers;
 use Illuminate\Support\ServiceProvider;
 
 use HaydenPierce\ClassFinder\ClassFinder;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Cache;
 use Netflex\Structure\Structure;
 use Throwable;
 
@@ -13,13 +15,20 @@ class StructureServiceProvider extends ServiceProvider
 {
     public function boot ()
     {
-        $models = ClassFinder::getClassesInNamespace('App\Models', ClassFinder::RECURSIVE_MODE);
-        
+        $models = [];
+        $classFinder = fn () => ClassFinder::getClassesInNamespace('App\Models', ClassFinder::RECURSIVE_MODE);
+
+        if(in_array(App::environment(), ['master', 'dev'])) {
+          $models = Cache::rememberForever('netflex/structure/models', $classFinder);
+        } else {
+          $models = $classFinder();
+        }
+
         foreach ($models as $model) {
             try {
-                if (!Structure::isModelRegistered($model)) {
-                    Structure::registerModel($model);
-                }
+              if (!Structure::isModelRegistered($model)) {
+                Structure::registerModel($model);
+              }
             } catch (Throwable $e) {
                 continue;
             }
