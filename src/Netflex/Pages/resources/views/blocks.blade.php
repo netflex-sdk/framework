e716f01102c7a88dc1afeca9676ce264817e81a4
Tommy Stigen Olsen <tommysolsen@gmail.com>
2024-08-13 15:47:12 +0200

    Implement attribute expansion for variables in $block
    Adds a setting which will use an alternate x-blocks implementation which sends all attributes it receives to any subcomponent.

--- src/Netflex/Pages/resources/views/blocks.blade.php
+++ src/Netflex/Pages/resources/views/blocks.blade.php
@@ -1,8 +1,24 @@
-@php($originalHash = blockhash())
+@php
+  $originalHash = blockhash()
+@endphp
 @foreach($blocks as $block)
-  @php(@list($component, $hash) = $block)
-  @php(blockhash($hash))
-  <x-dynamic-component :component="$component" :variables="$variables" />
+
+  @php
+    @list($component, $hash) = $block;
+    blockhash($hash)
+  @endphp
+
+  @if(config('pages.expand-attributes', false))
+    @php
+      /** @var \Illuminate\View\ComponentAttributeBag $attributes2 */
+      $attributes2 = new \Illuminate\View\ComponentAttributeBag();
+      $attributes2->setAttributes($__data);
+      $attributes2 = $attributes2->whereDoesntStartWith("__");
+    @endphp
+    <x-dynamic-component :component="$component" :attributes="$attributes2" />
+  @else
+    <x-dynamic-component :component="$component" :variables="$variables"/>
+  @endif
   @php(blockhash(null))
 @endforeach
 
