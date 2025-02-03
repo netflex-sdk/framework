2d6709843dfba8e35e415374a3be7ee92738afc6
Cathrine Vaage <cathrine.vaage@gmail.com>
2025-02-03 13:44:28 +0100

    fix(commerce): fix sending partial data and properties

--- src/Netflex/Commerce/Data.php
+++ src/Netflex/Commerce/Data.php
@@ -8,4 +8,13 @@ use Netflex\Commerce\Traits\API\DataAPI;
 class Data extends ReactiveObject
 {
   use DataAPI;
+
+  public function toModifiedArray(): array
+  {
+    if (!empty($this->modified)) {
+      return json_decode(json_encode($this->attributes), true);
+    }
+
+    return [];
+  }
 }
--- src/Netflex/Commerce/Properties.php
+++ src/Netflex/Commerce/Properties.php
@@ -8,29 +8,38 @@ use Netflex\Support\ReactiveObject;
 
 class Properties extends ReactiveObject implements Arrayable
 {
-    protected $readOnlyAttributes = [];
+  protected $readOnlyAttributes = [];
 
-    use Localizable;
+  use Localizable;
 
-    /**
-     * @return array
-     */
-    #[\ReturnTypeWillChange] 
-    public function jsonSerialize()
-    {
-        $attributes = empty($this->attributes) ? [] : $this->attributes;
+  /**
+   * @return array
+   */
+  #[\ReturnTypeWillChange]
+  public function jsonSerialize()
+  {
+    $attributes = empty($this->attributes) ? [] : $this->attributes;
 
-        foreach ($attributes as $key => $value) {
-            if (!is_scalar($value)) {
-                unset($attributes[$key]);
-            }
-        }
-
-        return $attributes;
+    foreach ($attributes as $key => $value) {
+      if (!is_scalar($value)) {
+        unset($attributes[$key]);
+      }
     }
 
-    public function toArray(): array
-    {
-        return $this->attributes;
+    return $attributes;
+  }
+
+  public function toArray(): array
+  {
+    return $this->attributes;
+  }
+
+  public function toModifiedArray(): array
+  {
+    if (!empty($this->modified)) {
+      return json_decode(json_encode($this->attributes), true);
     }
+
+    return [];
+  }
 }
